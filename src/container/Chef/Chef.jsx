49f36b8e2b47bb9c2fd9e60dfd0940a9d7b7a6ac
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-03-20 14:06:03 +0300

    build and style chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper__img-reverse'>
+      <img src={images.chef} alt="chef"/>
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading 
+        title="Chef's word"
+      />
+      <h1 className='headtext__cormorant'>What we believe in</h1>
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote"/>
+          <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        </div>
+        <p className='p__opensans'>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign"/>
+      </div>
+    </div>
   </div>
 );
 
